411e0e490e1f91d3f266d55160e1441b7b3c3e00
Yosef Maldonado <yosefmarr@gmail.com>
2021-11-02 18:51:58 -0600

    Cambios 2 de Noviembre

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -12,7 +12,62 @@ const Dashboard = () => {
   const tipoDeCuentaRef = useRef();
   const monedaRef = useRef();
 
-  console.log(authCtx.currentUser);
+  //console.log(authCtx.currentUser);
+
+  const createAccount = async (body) => {
+    const idToken = await authCtx.currentUser.getIdToken();
+    let response = await fetch(`${process.env.REACT_APP_BACKEND_BASE_URL}/account`, {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json',
+        'Authorization': `Bearer ${idToken}`
+      },
+      body: JSON.stringify(body)
+    });
+    await response.json();
+  }
+
+  const updateAllUserAccounts = async () => {
+    const idToken = await authCtx.currentUser.getIdToken();
+    let response = await fetch(`${process.env.REACT_APP_BACKEND_BASE_URL}/account`, {
+      method: 'GET',
+      headers: {
+        'Authorization': `Bearer ${idToken}`
+      }
+    });
+    const accounts = await response.json();
+    return accounts;
+  }
+
+  useEffect(() => {
+    const getCurrenciesAndAccountTypes = async () => {
+      const idToken = await authCtx.currentUser.getIdToken();
+      let reponse = await fetch(`${process.env.REACT_APP_BACKEND_BASE_URL}/currency`, {
+                              method: 'GET',
+                              headers: {
+                                'Authorization': `Bearer ${idToken}`
+                              }
+                          });
+      let currencies = await reponse.json();
+      reponse = await fetch(`${process.env.REACT_APP_BACKEND_BASE_URL}/account-type`, {
+                              method: 'GET',
+                              headers: {
+                                'Authorization': `Bearer ${idToken}`
+                              }
+                          });
+      let accountTypes = await reponse.json();
+      setCurrencies(currencies.data);
+      setAccountTypes(accountTypes.data);
+    }
+
+    const getAllUserAccounts = async () => {
+      const accounts = await updateAllUserAccounts();
+      setUserAccounts(accounts.data);
+    }
+    
+    getCurrenciesAndAccountTypes();
+    getAllUserAccounts();
+  }, [authCtx]);
 
   const onSubmitHandler = async (e) => {
     e.preventDefault();
@@ -20,16 +75,48 @@ const Dashboard = () => {
     if(tipoDeCuentaRef.current.value === '') return;
     if(monedaRef.current.value === '') return;
     setDisableSubmit(true)
-    // ...
+    const description = nombreCuentaRef.current.value;
+    const account_type = Number(tipoDeCuentaRef.current.value);
+    const currency = Number(monedaRef.current.value);
+    try {
+      await createAccount({ description, account_type, currency });
+      const accounts = await updateAllUserAccounts();
+      setUserAccounts(accounts.data);
+    } catch (e) {
+      console.log(e);
+    }
     setDisableSubmit(false)
     nombreCuentaRef.current.value = '';
     tipoDeCuentaRef.current.value = '';
     monedaRef.current.value = '';
   }
 
-  const accountTypesOptions = null;
-  const currenciesOptions = null;
-  const userAccountsList = null;
+  const accountTypesOptions = accountTypes.map((option) => {
+    return <option
+                  key={option.account_type}
+                  value={option.account_type}>
+            {option.description}
+    </option>;
+  });
+  const currenciesOptions = currencies.map((option) => {
+    return <option
+                  key={option.currency}
+                  value={option.currency}>
+            {option.description}
+    </option>;
+  });
+  const userAccountsList = userAccounts.map((item) => {
+    return <ListGroup.Item key={item.account} action>
+      <Card>
+        <Card.Body>
+          <Card.Title>{item.description}</Card.Title>
+          <Card.Text>
+            <b>Tipo: </b> {item.account_type_description}, <b>Moneda :</b> {item.currency_description}
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    </ListGroup.Item>
+  });
 
   return (
     <Container fluid className="mt-5">
